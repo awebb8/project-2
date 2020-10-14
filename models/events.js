78f177e38b1dc3d5e98073e34f2c8c4629c8e2bc
Developer Raj <rajdeepkakar@gmail.com>
2020-10-14 16:44:24 -0400

    Added the events.js file to models folder. Defined Event table with 5 fields. Did not incldue the customer_id foreign relationship. Assoicated the event object with the Service object.

---
+++ models/events.js
@@ -0,0 +1,31 @@
+module.exports = function (sequelize, DataTypes) {
+	const Event = sequelize.define(
+		"Event",
+		{
+			// Giving the event  model a name of type STRING
+			name: DataTypes.STRING,
+		},
+		{
+			date: DataTYPES.DATE,
+		},
+		{
+			startTime: DataTYPES.TIME,
+		},
+		{
+			endTime: DataTYPES.TIME,
+		},
+		{
+			eventType: DataTYPES.STRING,
+		}
+	);
+
+	Event.associate = function (models) {
+		// Associating Event with Services
+		// When an Event is deleted, also delete any associated Services
+		Event.hasMany(models.Service, {
+			onDelete: "cascade",
+		});
+	};
+
+	return Event;
+};
