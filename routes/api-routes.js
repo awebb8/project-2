37f475856ea6b50dfc129670105ffdb73d3b5784
Alkawe <alixkwebb@gmail.com>
2020-10-16 13:44:59 -0400

    added customer-login and vendor-login js files, modified related handlebars files, and added routes to the files

--- routes/api-routes.js
+++
@@ -1,9 +0,0 @@
-const db = require("../models");
-const passport = require("../config/passport");
-
-module.exports = function (app) {
-	app.post("/api/login", passport.authenticate("local"), function (req, res) {
-		res.json(req.customer);
-	});
-};
-
