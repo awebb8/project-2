766cd47092182523361282aad3180cec851f70fa
Alkawe <alixkwebb@gmail.com>
2020-10-19 00:08:17 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,24 @@
 require("dotenv").config();
 
-module.exports = {
-	development: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: "culturalbash",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
-	test: {
-		username: "root",
-		password: null,
-		database: "database_test",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
-	production: {
-		username: "root",
-		password: null,
-		database: "database_production",
-		host: "127.0.0.1",
-		dialect: "mysql",
-	},
+const config = {
+  development: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "culturalbash",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
 };
+
+module.exports = config;
