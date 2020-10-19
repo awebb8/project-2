f9ad9e49f8a287e31a5ba8ca0475d6dbf0ef404a
Alkawe <alixkwebb@gmail.com>
2020-10-18 21:11:48 -0400

    added state to vendors table and added data to vendor page

--- models/vendor.js
+++ models/vendor.js
@@ -2,6 +2,7 @@ module.exports = function (sequelize, DataTypes) {
 	var Vendor = sequelize.define("Vendor", {
 		business_Name: DataTypes.STRING,
 		category: DataTypes.STRING,
+		state: DataTypes.STRING,
 		first_Name: DataTypes.STRING,
 		last_Name: DataTypes.STRING,
 		email: DataTypes.STRING,
